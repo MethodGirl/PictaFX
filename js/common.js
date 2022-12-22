58b7afe058e0eb96f2ea35b2d6d92ba7de40df5a
MethodGirl <94073698+MethodGirl@users.noreply.github.com>
2022-12-22 03:06:29 +0300

    768px

---
+++ js/common.js
@@ -0,0 +1,14 @@
+$(function () {
+    
+    $(".hamburger").click(function () {
+        $(this).toggleClass("is-active");
+
+        if ($(this).hasClass("is-active")) {
+            $(".main-nav__list").slideDown(300);
+        } else {
+            $(".main-nav__list").slideUp(300);
+        }
+    }
+    
+    )});
+
